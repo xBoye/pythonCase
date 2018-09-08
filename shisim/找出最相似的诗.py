4c0eca02a32418131dcdc6bdd7942c5bbffdfcdf
xboye <1289572012@qq.com>
2018-09-09 00:45:21 +0800

    g3: 诗相似度

--- shisim/找出最相似的诗.py
+++ shisim/找出最相似的诗.py
@@ -5,15 +5,13 @@ Created on Fri Sep  7 21:46:19 2018
 @author: ff7f
 """
 """
-数据集： 七律.txt
-测试集： cases.txt   # 建立一个小型的子集进行算法测试
-
+训练数据集： 七律.txt
 处理流程：
 1.读入文本，生成诗列表；
-2.分词，基于诗文文言的特殊性，使用12,34,56,67格式进行分词；
+2.分词，基于七律诗特点，按12,34,56,67格式进行分词；
 3.文档向量化，使用tfidf向量方法；
-4.循环计算余弦相似度，生成相似字典或矩阵；
-5.根据相似矩阵，找出最相似的两首诗。
+4.随机抽取批次，循环计算余弦相似度，生成相似字典；
+5.相似字典数据按相似度逆序排列，找出最相似的两首诗。
 """
 import re
 from tqdm import tqdm  # 进度条
@@ -53,7 +51,7 @@ for s in s_train:
 from sklearn.feature_extraction.text import TfidfVectorizer as TV #tfidf向量
 
 model = TV()  #tfidf向量
-vecs_train = model.fit_transform(corpus)   # 稀疏矩阵 防止溢出 生成特征向量矩阵，每行对应一首诗，每列对应一个特征词
+vecs_train = model.fit_transform(corpus)   # 稀疏矩阵 防止溢出 特征向量矩阵，每行对应一首诗，每列对应一个特征词
 vocab_train = model.get_feature_names()
 vocab_size = len(vocab_train)   # 词汇表size
 #print(f'  词汇表size：{vocab_size}')
@@ -63,14 +61,14 @@ import numpy as np
 train_size = len(s_train)   # 训练集size: 总32196首
 batch_size = 200            # 批次包含文档数
 
-batch_mask = np.random.choice(train_size, batch_size, replace=False)    # 随机抽取batch_size首
-vecs = vecs_train[batch_mask]
+batch_mask = np.random.choice(train_size, batch_size, replace=False)    
+vecs = vecs_train[batch_mask]   # 从特征向量矩阵随机抽取batch_size首
 
 #4.计算余弦相似度，生成相似字典，数据集大，余弦相似度速度慢，corpus取子集测试
 from scipy.spatial.distance import cosine
 
 print('> 正在计算相似度，请稍等。。')
-n = batch_size  #文档数
+n = batch_size  # 批次文档数
 simDict = {}   # 相似度字典结构
 for i in tqdm(range(n-1)):      # tqdm显示进度条
     for j in range(i+1, n):
@@ -83,7 +81,7 @@ simDict_sorted = sorted(simDict.items(),key=lambda item:item[1],reverse=True)
 i,j = map(int,re.findall('\d+',simDict_sorted[0][0])) 
 result = list(map(f, simDict_sorted))
 
-# 显示结果print(f'  ')
+# 显示结果
 print(f'\n\n> 训练集size：{train_size}，词汇表size：{vocab_size}，本批文档数：{batch_size}')
 print(f'> 最相似的两首诗：{result[0]}')
 print(f'> 第{i}首诗  {s_train[i]}')
